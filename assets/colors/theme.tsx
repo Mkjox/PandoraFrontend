156633fdae6c64ec1d36918c4e0aa9cfd614cd6f
jox <zincir_890@hotmail.com>
2024-11-06 13:35:31 +0300

    Created PassApp

---
+++ assets/colors/theme.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export const lightTheme = StyleSheet.create({
+    container: {
+        backgroundColor: '#FFFFFF',
+    },
+    button: {
+        backgroundColor: '#756EF3'
+    },
+    buttonText: {
+        color: '#FFFFFF'
+    },
+    iconBackgroundSquare: {
+        backgroundColor: '#FFFFFF',
+        borderColor: '#E9F1FF',
+        borderWidth: 1
+    },
+    iconBackgroundCircle: {
+        backgroundColor: '#FFFFFF',
+        borderColor: '#E9F1FF',
+        borderWidth: 1,
+    }
+})
